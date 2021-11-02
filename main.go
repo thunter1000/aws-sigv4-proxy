57289d65c2959844b792228312faaf13851ff1df
Thomas Hunter <thunter1000@gmail.com>
2021-11-02 17:07:46 +0000

    fix: disable redirect handling

--- main.go
+++ main.go
@@ -104,6 +104,12 @@ func main() {
 		}
 	})
 
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
 	log.WithFields(log.Fields{"StripHeaders": *strip}).Infof("Stripping headers %s", *strip)
 	log.WithFields(log.Fields{"port": *port}).Infof("Listening on %s", *port)
 
@@ -111,7 +117,7 @@ func main() {
 		http.ListenAndServe(*port, &handler.Handler{
 			ProxyClient: &handler.ProxyClient{
 				Signer:              signer,
-				Client:              http.DefaultClient,
+				Client:              client,
 				StripRequestHeaders: *strip,
 				SigningNameOverride: *signingNameOverride,
 				HostOverride:        *hostOverride,
